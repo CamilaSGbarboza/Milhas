a8db06d1beb65866d5a52311d7f3d3328a2a7b0a
camila <camilasangobar@gmail.com>
2021-03-21 20:18:30 -0300

    milhas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Milhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---Milhas---");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("digite um número:");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            double km = m * 1609;
+            Console.WriteLine($"É {km} km");
+
+
+        }
+    }
+}
